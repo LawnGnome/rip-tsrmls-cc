21df0b211625ccd564fd64861634208ae39c9a64
Adam Harvey <aharvey@php.net>
2016-02-16 18:44:57 +0000

    Updates.

--- js/images.js
+++ js/images.js
@@ -7,9 +7,17 @@
       var list = $("<ul class='image-credits' />").appendTo(container);
 
       $.each(data, function () {
+        if ("" == this.author) {
+          return;
+        }
+
         var item = $("<li />").appendTo(list);
 
-        $("<a />").attr("href", this.source).text(this.title + " by " + this.author).appendTo(item);
+        if ("source" in this) {
+          $("<a class='title' />").attr("href", this.source).text(this.title + " by " + this.author).appendTo(item);
+        } else {
+          $("<span class='title' />").text(this.title + " by " + this.author).appendTo(item);
+        }
         $("<span class='licence' />").text(this.licence).appendTo(item);
       });
     });
